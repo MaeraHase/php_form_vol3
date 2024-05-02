4d5e698eeb8086beeafe7eff58cd983eb31753a5
MaeraHase <pink19990123@gmail.com>
2024-05-02 12:44:42 +0900

    修正１

--- funcs.php
+++ funcs.php
@@ -13,8 +13,8 @@ function db_conn(){
       $db_pw =""; //パスワード
       $db_host = "localhost"; //DBホスト
       return new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host,$db_id,$db_pw);
-
     
+      
 
     } catch (PDOException $e) {
       exit('DB_CONECT:'.$e->getMessage());
